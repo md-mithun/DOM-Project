3649024d9b8820651e7ee72916a785f77c4753de
md-mithun <mmithunali2@gmail.com>
2023-09-25 20:20:23 +0600

    new file added

---
+++ Project-5 Dynamic Toast Message/index.js
@@ -0,0 +1,58 @@
+// find dom element
+const colorButton = document.getElementById('color-btn');
+const copyButton = document.getElementById('copy-btn');
+const outPutTextBox = document.getElementById('output');
+//globals
+toast = null;
+
+//generate random color
+colorButton.addEventListener('click', function () {
+    const red = Math.floor(Math.random() * 254);
+    const green = Math.floor(Math.random() * 254);
+    const blue = Math.floor(Math.random() * 254);
+    const bgColor = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+    setDivColor(bgColor);
+
+    //set hex color as display 
+    outPutTextBox.value = bgColor.toUpperCase();
+});
+
+//set random color on main-div
+function setDivColor(bgColor) {
+    const mainDiv = document.querySelector('.main');
+    mainDiv.style.background = bgColor;
+}
+
+//copy color code 
+copyButton.addEventListener('click', function () {
+    const colorCode = outPutTextBox.value;
+    if (colorCode != '') {
+        window.navigator.clipboard.writeText(colorCode.toUpperCase());
+        // if one toast already here in body another toast wouldn't create
+        if (toast !== null) {
+            toast.remove();
+            toastMessage(colorCode.toUpperCase());
+        } else {
+            toastMessage(colorCode.toUpperCase());
+        }
+    }
+}
+)
+
+//generate toast message
+function toastMessage(message) {
+    toast = document.createElement('div');
+    toast.innerText = `${message} copied`;
+    toast.classList = 'toast toast-animation-in';
+    //remove toast by clicked in
+    toast.addEventListener('click', function () {
+        toast.classList.remove('toast-animation-in');
+        toast.classList.add('toast-animation-out');
+
+        toast.addEventListener('animationend', function () {
+            toast.remove();
+        }
+        )
+    })
+    document.body.appendChild(toast);
+}
